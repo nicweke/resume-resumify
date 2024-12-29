bc7ef92ab54783ec633b1e0d4d2cd9b57ad8162a
Nick <nicweke@gmail.com>
2024-12-29 10:51:51 +0300

    second commit

--- lib/redux/settingsSlice.ts
+++ lib/redux/settingsSlice.ts
@@ -1,4 +1,4 @@
-
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
 export interface Settings {
   themeColor: string;
@@ -11,14 +11,14 @@ export interface Settings {
     projects: boolean;
     skills: boolean;
     custom: boolean;
-  }
+  };
   formToHeading: {
     workExperiences: string;
     educations: string;
     projects: string;
     skills: string;
     custom: string;
-  }
+  };
   formsOrder: ShowForm[];
   showBulletPoints: {
     workExperiences: boolean;
@@ -26,17 +26,69 @@ export interface Settings {
     projects: boolean;
     skills: boolean;
     custom: boolean;
-  }
+  };
 }
 
 export type ShowForm = keyof Settings["formToShow"];
 export type FormWithBulletPoints = keyof Settings["showBulletPoints"];
-export type GeneralSettings = Exclude<
-keyof Settings,
-"formToShow" | "formToHeading" | "formsOrder" | "showBulletPoints"
->
+export type GeneralSetting = Exclude<
+  keyof Settings,
+  "formToShow" | "formToHeading" | "formsOrder" | "showBulletPoints"
+>;
 
 export const DEFAULT_THEME_COLOR = "#38bdf8";
 export const DEFAULT_FONT_FAMILY = "Roboto";
 export const DEFAULT_FONT_SIZE = "11";
 export const DEFAULT_FONT_COLOR = "171717";
+
+export const initialSettings: Settings = {
+  themeColor: DEFAULT_THEME_COLOR,
+  fontFamily: DEFAULT_FONT_FAMILY,
+  fontSize: DEFAULT_FONT_SIZE,
+  documentSize: "Letter",
+  formToShow: {
+    workExperiences: true,
+    educations: true,
+    projects: true,
+    skills: true,
+    custom: true,
+  },
+  formToHeading: {
+    workExperiences: "WORK EXPERIENCE",
+    educations: "EDUCATION",
+    projects: "PROJECT",
+    skills: "SKILLS",
+    custom: "CUSTOM SECTION",
+  },
+  formsOrder: ["workExperiences", "educations", "projects", "skills", "custom"],
+
+  showBulletPoints: {
+    workExperiences: true,
+    educations: true,
+    projects: true,
+    skills: true,
+    custom: true,
+  },
+};
+
+export const settingsSlice = createSlice({
+  name: "settings",
+  initialState: initialSettings,
+  reducers: {
+    changeSettings: (
+      draft,
+      action: PayloadAction<{ field: GeneralSetting; value: string }>
+    ) => {
+      const { field, value } = action.payload;
+      draft[field] = value;
+    },
+    changeShowForm: (
+      draft,
+      action: PayloadAction<{ field: ShowForm; value: boolean }>
+    ) => {
+      const { field, value } = action.payload;
+      draft.formToShow[field] = value;
+    },
+
+  },
+});
